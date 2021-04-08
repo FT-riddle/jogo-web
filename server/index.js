887d802adb6ada107e69c9fde8ab7da28e2a0593
Francinerd <francineregisduarte@gmail.com>
2021-04-08 18:47:55 -0300

    francine

--- server/index.js
+++ server/index.js
@@ -5,5 +5,5 @@ const server = require("http").Server(app);
 
 const PORT = 3666;
 
-app.use(express.static("./"));
+app.use(express.static("./jogo-web"));
 server.listen(PORT, () => console.log(`Server listening on port ${PORT}!`));
