08ef8296d9247592f80fc5d11c4e48ac182d5dff
Thiago-Paim-Alflen <74904175+Thiago-Paim-Alflen@users.noreply.github.com>
2021-04-11 10:53:27 -0300

    Update index.js
    Adicionando VOIP no jogo

--- server/index.js
+++ server/index.js
@@ -7,3 +7,19 @@ const PORT = 3666;
 
 app.use(express.static("./"));
 server.listen(PORT, () => console.log(`Server listening on port ${PORT}!`));
+
+ // Sinalização de áudio: oferta
+  socket.on("offer", (socketId, description) => {
+    socket.to(socketId).emit("offer", socket.id, description);
+  });
+
+  // Sinalização de áudio: atendimento da oferta
+  socket.on("answer", (socketId, description) => {
+    socket.to(socketId).emit("answer", description);
+  });
+
+  // Sinalização de áudio: envio dos candidatos de caminho
+  socket.on("candidate", (socketId, signal) => {
+    socket.to(socketId).emit("candidate", signal);
+  });
+
