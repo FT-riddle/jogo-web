600db203df743dfdb51ef0ce104c700d9e1ae2b0
Francinerd <francineregisduarte@gmail.com>
2021-04-13 13:32:31 -0300

    francine

--- servidor/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const app = express();
-const server = require("http").Server(app);
-
-const PORT = 3666;
-
-app.use(express.static("./"));
-server.listen(PORT, () => console.log(`Server listening on port ${PORT}!`));
